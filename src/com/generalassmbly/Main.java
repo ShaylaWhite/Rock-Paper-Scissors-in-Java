a2f585ea1271d714a10436d085d9fcedefd19253
Shayla <shayladwhite1@gmail.com>
2023-09-04 16:33:54 -0700

    added in Main Class Objectives and Outline for the main Class

---
+++ src/com/generalassmbly/Main.java
@@ -0,0 +1,29 @@
+// CLIENT/DRIVER CLASS
+
+/**
+ ** Main Class (Client/Driver Class)**
+ * Main method and game flow handling.
+ * Uses other classes to create objects and manage the game.
+ * Usage of OOP: Control class, Composition (uses other classes).
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        // Initialize the game components and objects here
+
+        // Display the main menu to the user
+
+        // Handle user input and navigate through the game menu
+
+        // Implement game logic and play the game
+
+        // Manage game history
+
+        // Handle user input for quitting the game and display final results
+
+        // Optionally, consider adding exception handling and testing
+
+
+    }
+}
+
