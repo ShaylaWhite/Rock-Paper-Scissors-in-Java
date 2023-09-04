3d97ff954ae23fc3c8c460d3bd04bd8d3d07be3d
Shayla <shayladwhite1@gmail.com>
2023-09-04 16:36:46 -0700

    added in package

--- src/com/generalassmbly/Main.java
+++ src/com/generalassmbly/Main.java
@@ -1,11 +1,12 @@
-// CLIENT/DRIVER CLASS
-
+package com.generalassembly;
 /**
  ** Main Class (Client/Driver Class)**
  * Main method and game flow handling.
  * Uses other classes to create objects and manage the game.
  * Usage of OOP: Control class, Composition (uses other classes).
  */
+
+//CLIENT/DRIVER CLASS
 public class Main {
     public static void main(String[] args) {
 
