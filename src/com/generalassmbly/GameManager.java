46c7e0eb807eb6b48c0819f29fed806c9f41e3e1
Shayla <shayladwhite1@gmail.com>
2023-09-04 18:58:22 -0700

    created the big dawg playgame() method

--- src/com/generalassmbly/GameManager.java
+++ src/com/generalassmbly/GameManager.java
@@ -13,4 +13,23 @@ public class GameManager {
         this.gameHistory = gameHistory;
         this.validator = validator;
     }
+    // Play Game Method used in the called in GameMenu class
+    public void playGame() {
+        // Get moves from the players
+        String humanMove = humanPlayer.makeMove();
+        String computerMove = computerPlayer.makeMove();
+
+        // Determine the winner
+        String result = determineWinner(humanMove, computerMove);
+
+        // Update player scores
+        updateScores(result);
+
+        // Record the game outcome in the game history
+        gameHistory.recordGameResult(humanPlayer.getName(), computerPlayer.getName(), result);
+
+        // Display the game result to the players
+        displayGameResult(humanMove, computerMove, result);
+    }
+
 }
