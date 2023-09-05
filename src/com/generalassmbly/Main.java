c5eb07f309a352413d36a421643a128304e10722
Shayla <shayladwhite1@gmail.com>
2023-09-04 19:20:23 -0700

    main method flow created for flw of gamelogic

--- src/com/generalassmbly/Main.java
+++ src/com/generalassmbly/Main.java
@@ -9,8 +9,25 @@ package com.generalassmbly;
 public class Main {
     // CLIENT/DRIVER CLASS
     public static void main(String[] args) {
+        // Create instances of the necessary classes
+        Player humanPlayer = new HumanPlayer("Player 1"); // You can specify the player's name
+        Player computerPlayer = new ComputerPlayer("Computer");
 
+        // Create a game history instance
+        GameHistory gameHistory = new GameHistory();
 
+        // Create a game manager instance
+        GameManager gameManager = new GameManager(humanPlayer, computerPlayer, gameHistory, new Validator());
 
+        // Create a game menu instance
+        GameMenu gameMenu = new GameMenu(gameManager);
+
+        // Display the main menu and handle user choices
+        while (true) {
+            gameMenu.displayMainMenu();
+            String userInput = gameMenu.getUserInput();
+            gameMenu.handleUserChoice(userInput);
+        }
     }
+
 }
