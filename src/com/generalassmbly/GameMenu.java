51af8e5cc85fdb114689ad5834a8f2aa85dc9053
Shayla <shayladwhite1@gmail.com>
2023-09-04 18:45:05 -0700

    added private fields

--- src/com/generalassmbly/GameMenu.java
+++ src/com/generalassmbly/GameMenu.java
@@ -2,6 +2,9 @@ package com.generalassmbly;
 /**
  * GameMenu.java
  * Manages the main menu and user interactions for the Rock, Paper, Scissors game.
+ *  private is a key aspect of encapsulation, which is one of the fundamental principles of OOP. It helps ensure that the internal state of objects is managed and accessed in a controlled and consistent manner.
  */
 public class GameMenu {
+    private GameManager gameManager;
+    private Scanner scanner;
 }
