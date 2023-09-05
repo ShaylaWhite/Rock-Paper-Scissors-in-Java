66c329d234273ff1dbc1f1ca9b91c115fae048c9
Shayla <shayladwhite1@gmail.com>
2023-09-04 17:05:47 -0700

    created Player and Human Player Class

---
+++ src/com/generalassmbly/HumanPlayer.java
@@ -0,0 +1,4 @@
+package com.generalassmbly;
+
+public class HumanPlayer {
+}
--- src/com/generalassmbly/Main.java
+++ src/com/generalassmbly/Main.java
@@ -1,15 +1,13 @@
 package com.generalassmbly;
-
-// CLIENT/DRIVER CLASS
-
 /**
  * Main Class (Client/Driver Class):
- *
  * Main method and game flow handling.
  * Uses other classes to create objects and manage the game.
  * Usage of OOP: Control class, Composition (uses other classes).
  */
+
 public class Main {
+    // CLIENT/DRIVER CLASS
     public static void main(String[] args) {
         // Initialize the game components and objects here
 
