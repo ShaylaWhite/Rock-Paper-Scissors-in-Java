67deba724bd8f673339fa930188a704432e10689
Shayla <shayladwhite1@gmail.com>
2023-09-04 17:41:50 -0700

    wroteout step by step plan, created subclasses of player >Human / >Computer Player

--- src/com/generalassmbly/ComputerPlayer.java
+++ src/com/generalassmbly/ComputerPlayer.java
@@ -1,4 +1,8 @@
 package com.generalassmbly;
+// ComputerClass.java
+public class ComputerPlayer extends Player {
+    // Constructor
+    public ComputerPlayer(String name) {
 
-public class ComputerPlayer {
+    }
 }
