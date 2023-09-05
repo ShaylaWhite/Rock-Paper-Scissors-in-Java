5846118e12cb5c35542382cea34de31768e93b7e
Shayla <shayladwhite1@gmail.com>
2023-09-04 17:47:30 -0700

    added in javadoc comments

--- src/com/generalassmbly/ComputerPlayer.java
+++ src/com/generalassmbly/ComputerPlayer.java
@@ -1,5 +1,11 @@
 package com.generalassmbly;
 // ComputerClass.java
+
+/**
+ * ComputerPlayer.java
+ * Represents a computer player in the Rock, Paper, Scissors game.
+ * Inherits from the Player class.
+ */
 public class ComputerPlayer extends Player {
     // Constructor
     public ComputerPlayer(String name) {
